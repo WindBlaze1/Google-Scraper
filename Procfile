c211d56e8d3a3f5df850c1490ca26733bbdca293
Harit <53652715+WindBlaze1@users.noreply.github.com>
2022-01-25 00:30:16 +0530

    'Commit'

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn proj01.wsgi --log-file -
--- proj01/settings.py
+++ proj01/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+m3=xjrf!d9p!uqwfi@d5jw!=w-keh8_@=37^oa*o(i2f!xg-t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scrape-google.herokuapp.com','127.0.0.1']
 
 
 # Application definition
